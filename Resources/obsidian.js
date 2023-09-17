53584a8a2d954cb9c4fe344f69a612471fc55d42
Joshua Coles <josh@coles.to>
2023-09-17 22:53:37 +0100

    Format and move the formatDate function to obsidian.js

--- Resources/obsidian.js
+++ Resources/obsidian.js
@@ -1,47 +1,57 @@
 /* eslint-disable no-bitwise, no-plusplus */
+function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
 
 (() => {
-  const dependencyLibrary = new PlugIn.Library(new Version('1.0'));
+    const dependencyLibrary = new PlugIn.Library(new Version('1.0'));
 
-  const vault = encodeURIComponent("My Life");
+    const vault = encodeURIComponent("My Life");
 
-  dependencyLibrary.getJoinery = async function getJoinery(projectId) {
-    console.log(`Finding joinery for ${projectId}`);
-    const request = new URL.FetchRequest();
-    request.method = 'GET';
-    request.url = URL.fromString(`http://localhost:9002/joinery/projects`);
-    const response = await request.fetch();
+    dependencyLibrary.getJoinery = async function getJoinery(projectId) {
+        console.log(`Finding joinery for ${projectId}`);
+        const request = new URL.FetchRequest();
+        request.method = 'GET';
+        request.url = URL.fromString(`http://localhost:9002/joinery/projects`);
+        const response = await request.fetch();
 
-    if (response.statusCode !== 200) {
-      throw buildErrorObject(response);
+        if (response.statusCode !== 200) {
+            throw buildErrorObject(response);
+        }
+
+        console.log(response.bodyString)
+        const projects = JSON.parse(response.bodyString);
+
+        if (projectId) {
+            const projectLink = `omnifocus:///task/${encodeURIComponent(projectId)}`;
+            return projects.find((p) => {
+                console.log(`Considering: ${p.omnifocusProjectLink} === ${projectLink}`)
+                return p.omnifocusProjectLink === projectLink
+            });
+        } else {
+            return projects;
+        }
+    };
+
+    dependencyLibrary.dailyNoteLink = function dailyNoteLink(date) {
+        return `[[${formatDate(date)}]]`;
     }
 
-    console.log(response.bodyString)
-    const projects = JSON.parse(response.bodyString);
+    dependencyLibrary.openObsidianNote = function openObsidianNote(obsidianFilePath) {
+        const encodedFilePath = encodeURIComponent(obsidianFilePath);
+        console.log(`Opening ${`obsidian://vault/${vault}/${encodedFilePath}`}`)
+        URL.fromString(`obsidian://vault/${vault}/${encodedFilePath}`).open();
+    };
 
-    if (projectId) {
-      const projectLink = `omnifocus:///task/${encodeURIComponent(projectId)}`;
-      return projects.find((p) => {
-        console.log(`Considering: ${p.omnifocusProjectLink} === ${projectLink}`)
-        return p.omnifocusProjectLink === projectLink
-      });
-    } else {
-      return projects;
-    }
-  };
+    dependencyLibrary.appendToObsidianNote = function openObsidianNote(obsidianFilePath, content) {
+        const encodedFilePath = encodeURIComponent(obsidianFilePath);
+        const encodedContent = encodeURIComponent(content);
+        const appendURI = `obsidian://advanced-uri?vault=${vault}&filepath=${encodedFilePath}&mode=append&data=${encodedContent}`;
+        URL.fromString(appendURI).open();
+    };
 
-  dependencyLibrary.openObsidianNote = function openObsidianNote(obsidianFilePath) {
-    const encodedFilePath = encodeURIComponent(obsidianFilePath);
-    console.log(`Opening ${`obsidian://vault/${vault}/${encodedFilePath}`}`)
-    URL.fromString(`obsidian://vault/${vault}/${encodedFilePath}`).open();
-  };
-
-  dependencyLibrary.appendToObsidianNote = function openObsidianNote(obsidianFilePath, content) {
-    const encodedFilePath = encodeURIComponent(obsidianFilePath);
-    const encodedContent = encodeURIComponent(content);
-    const appendURI = `obsidian://advanced-uri?vault=${vault}&filepath=${encodedFilePath}&mode=append&data=${encodedContent}`;
-    URL.fromString(appendURI).open();
-  };
-
-  return dependencyLibrary;
+    return dependencyLibrary;
 })();
--- Resources/reviewProject.js
+++ Resources/reviewProject.js
@@ -1,40 +1,40 @@
 function formatDate(date) {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
 }
 
 (() => {
-  // Main action
-  const action = new PlugIn.Action(async function reviewProject(
-    selection,
-  ) {
-    const project = selection.projects[0];
-    const projectJoinery = await this.common.getJoinery(project.id.primaryKey);
+    // Main action
+    const action = new PlugIn.Action(async function reviewProject(
+        selection,
+    ) {
+        const project = selection.projects[0];
+        const projectJoinery = await this.common.getJoinery(project.id.primaryKey);
 
-    if (!projectJoinery) {
-      const projectLink = `omnifocus:///task/${encodeURIComponent(project.id.primaryKey)}`;
-      new Alert("Project file not found", "We couldn't find a project file in Obsidian associated with this project, double check that the file exits and has the correct omnifocus project URL in its properties").show()
-			console.error("Project file not found:");
-      console.error(projectLink);
-			console.error("In dataset:");
-      console.error(response.bodyString)
-      return;
-    }
+        if (!projectJoinery) {
+            const projectLink = `omnifocus:///task/${encodeURIComponent(project.id.primaryKey)}`;
+            new Alert("Project file not found", "We couldn't find a project file in Obsidian associated with this project, double check that the file exits and has the correct omnifocus project URL in its properties").show()
+            console.error("Project file not found:");
+            console.error(projectLink);
+            console.error("In dataset:");
+            console.error(response.bodyString)
+            return;
+        }
 
-    const obsidianFilePath = projectJoinery.filePath;
-    const appendContent = `\n\n## Review [[${formatDate(new Date())}]]\n\n`;
+        const obsidianFilePath = projectJoinery.filePath;
+        const appendContent = `\n\n## Review [[${formatDate(new Date())}]]\n\n`;
 
-    await this.common.appendToObsidianNote(obsidianFilePath, appendContent);
-    this.common.openObsidianNote(obsidianFilePath);
-  });
+        await this.common.appendToObsidianNote(obsidianFilePath, appendContent);
+        this.common.openObsidianNote(obsidianFilePath);
+    });
 
-  action.validate = function startTogglTimerValidate(selection) {
-    return selection.tasks.length === 0 &&
-      selection.projects.length === 1 &&
-      selection.projects[0].nextReviewDate < new Date();
-  };
+    action.validate = function startTogglTimerValidate(selection) {
+        return selection.tasks.length === 0 &&
+            selection.projects.length === 1 &&
+            selection.projects[0].nextReviewDate < new Date();
+    };
 
-  return action;
+    return action;
 })();
